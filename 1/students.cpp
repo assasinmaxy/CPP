061d26182859829b09146a85ced96928e8b10f8e
kiselev <kiselev@melsytech.com>
2019-06-13 17:02:13 +0300

    1

---
+++ 1/students.cpp
@@ -0,0 +1,57 @@
+/* students.cpp */
+#include <string>
+#include <fstream>
+#include "students.h"
+
+// Конструктор Students
+Students::Students(std::string name, std::string last_name)
+{
+	Students::set_name(name);
+	Students::set_last_name(last_name);
+}
+
+// Установка имени студента
+void Students::set_name(std::string student_name)
+{
+	Students::name = student_name;
+}
+
+// Получение имени студента
+std::string Students::get_name()
+{
+	return Students::name;
+}
+
+// Установка фамилии студента
+void Students::set_last_name(std::string student_last_name)
+{
+	Students::last_name = student_last_name;
+}
+
+// Получение фамилии студента
+std::string Students::get_last_name()
+{
+	return Students::last_name;
+}
+
+// Установка промежуточных оценок
+void Students::set_scores(int scores[])
+{
+	int sum = 0;
+	for (int i = 0; i < 5; ++i) {
+		Students::scores[i] = scores[i];
+		sum += scores[i];
+	}
+}
+
+// Установка среднего балла
+void Students::set_average_ball(float ball)
+{
+	Students::average_ball = ball;
+}
+
+// Получение среднего балла
+float Students::get_average_ball()
+{
+	return Students::average_ball;
+}
---
+++ 1/students.h
@@ -0,0 +1,36 @@
+/* students.h */
+#pragma once /* Защита от двойного подключения заголовочного файла */
+#include <string>
+
+class Students {
+public:
+	// Конструктор класса Students
+	Students(std::string, std::string);
+
+	// Установка имени студента
+	void set_name(std::string);
+	// Получение имени студента
+	std::string get_name();
+
+	// Установка фамилии студента
+	void set_last_name(std::string);
+	// Получение фамилии студента
+	std::string get_last_name();
+
+	// Установка промежуточных оценок
+	void set_scores(int[]);
+
+	// Установка среднего балла
+	void set_average_ball(float);
+	// Получение среднего балла
+	float get_average_ball();
+private:
+	// Промежуточные оценки
+	int scores[5];
+	// Средний балл
+	float average_ball;
+	// Имя
+	std::string name;
+	// Фамилия
+	std::string last_name;
+};
